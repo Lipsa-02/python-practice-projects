fd1bde38b5c9fc319eb61745c053a6f454e1135c
Lipsa Priyadarsini Nayak <lipsapriyadarsini212@gmail.com>
2026-01-14 13:42:37 +0530

    Delete Function directory

--- Function/Ex5.py
+++
@@ -1,6 +0,0 @@
-def funct():
-    print("WElcome to my python file ")
-
-if __name__=="__main__":
-    print("i am from after function definition")
-    funct()
--- Function/Ex6.py
+++
@@ -1,6 +0,0 @@
-def welcome():
-    print("welcome to function topic")
-#main program
-print("I am after function Definition")
-welcome()
-print("type of welcome :",type(welcome))
